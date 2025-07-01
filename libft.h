af7b7885f938f0d52eb29d063022991321e1ff13
Furiosso <dgimenodemanuel@gmail.com>
2025-07-01 17:44:19 +0200

    add isspace function

--- libft.h
+++ libft.h
@@ -37,6 +37,7 @@ int		ft_isalnum(int c);
 int		ft_isalpha(int c);
 int		ft_isascii(int c);
 int		ft_isdigit(int c);
+int		ft_isspace(int c);
 int		ft_isprint(int c);
 char	*ft_itoa(int n);
 char	*ft_itoa_p(int n);
